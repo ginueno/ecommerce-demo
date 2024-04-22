6aab5622c5dac244fc4f1ec032d902937897e4d9
PhucBV <ginuenosan@gmail.com>
2024-04-23 02:17:48 +0700

    modified:   src/config/appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://demo-ecommerce-backend-production.up.railway.app/"
+export const API_BASE_URL = "https://demo-ecommerce-backend-production.up.railway.app"
 
 const jwt = localStorage.getItem("jwt")
 
